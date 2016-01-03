b61942637c3157bcbe5cdf24ca012423c82dcc30
ChenJingLei <cjl4565@163.com>
2016-01-03 21:37:31 +0800

    ManageUser可以增删改

--- LoveEatDateSource/src/main/java/app/controllers/ManageUserController.java
+++ LoveEatDateSource/src/main/java/app/controllers/ManageUserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by cjl20 on 2016/1/3.
  */
@@ -38,7 +39,41 @@ public class ManageUserController {
         return updateUserStatus;
     }
 
+    @RequestMapping(name = "/ManageUser/addOpenIdToManageUser", method = RequestMethod.GET)
+    public UpdateUserStatus addOpenIdToManageUser(@RequestParam(value = "Id") Long id, @RequestParam(value = "OpenId") String OpenId) {
+        UpdateUserStatus updateUserStatus = new UpdateUserStatus("-1", "none");
+        try {
+            if (repository.exists(id)) {
+                ManageUser manageUser = repository.findOne(id);
+                manageUser.setOpenId(OpenId);
+                repository.save(manageUser);
+                updateUserStatus.setMsgCode("1");
+                updateUserStatus.setResult("successful");
+            } else {
+                updateUserStatus.setMsgCode("0");
+                updateUserStatus.setResult("user is not exists");
+            }
+        } catch (Exception e) {
+            updateUserStatus.setResult("failed:" + e.getMessage());
+        }
+        return updateUserStatus;
+    }
 
-
-
+    @RequestMapping(name = "/ManageUser/RemoveManageUser", method = RequestMethod.GET)
+    public UpdateUserStatus RemoveManageUser(@RequestParam(value = "Id") Long id) {
+        UpdateUserStatus updateUserStatus = new UpdateUserStatus("-1", "none");
+        try {
+            if (repository.exists(id)) {
+                repository.delete(id);
+                updateUserStatus.setMsgCode("1");
+                updateUserStatus.setResult("successful");
+            } else {
+                updateUserStatus.setMsgCode("0");
+                updateUserStatus.setResult("user is not exists");
+            }
+        } catch (Exception e) {
+            updateUserStatus.setResult("failed" + e.getMessage());
+        }
+        return updateUserStatus;
+    }
 }
